c5bf2153ae1fb36b2705fbcb759b7b94c6575f2e
Vahid KARABUDAK <vahidkarabudakitu@gmail.com>
2022-10-08 00:20:38 +0300

    Delete 01_SELECT.sql

--- SQL/01_SELECT.sql
+++
@@ -1,6 +0,0 @@
--- Yorum satırı
--- sql komutları büyük ya da küçük harf ayırt etmez
--- ; burada kullanılmak zorunda olmasa da farklı veritabanları kullandığı için kullanmak iyi olur.
--- SELECT title, description FROM film; -- title ve description sütunlarını çağırır
--- SELECT * From film; -- Bütün sütunları çağırır.
-SELECT first_name, last_name FROM actor; -- actor tablosundan first_name ve last_name sütunları çağırılıyor.
