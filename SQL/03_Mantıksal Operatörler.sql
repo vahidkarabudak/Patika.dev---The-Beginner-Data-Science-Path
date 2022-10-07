8493265d71f4568623e16deac8f4406256316d27
Vahid KARABUDAK <vahidkarabudakitu@gmail.com>
2022-10-08 00:20:52 +0300

    Delete 03_Mantıksal Operatörler.sql

--- SQL/03_Mantıksal Operatörler.sql
+++
@@ -1,7 +0,0 @@
--- SELECT * FROM actor WHERE first_name = 'Penelope' AND last_name = 'Monroe' ;
-
--- SELECT * FROM actor WHERE first_name = 'Penelope' OR first_name = 'Bob' ;
-
--- SELECT * FROM film WHERE NOT rental_rate = 4.99 ;
-
--- SELECT * FROM film WHERE NOT (rental_rate = 4.99 OR rental_rate = 2.99) ;
