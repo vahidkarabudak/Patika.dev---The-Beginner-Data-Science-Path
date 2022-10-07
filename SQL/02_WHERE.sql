3f02e0e05bd052d72e771bb8f310107858c87094
Vahid KARABUDAK <vahidkarabudakitu@gmail.com>
2022-10-08 00:19:35 +0300

    Add files via upload

---
+++ SQL/02_WHERE.sql
@@ -0,0 +1,16 @@
+-- replacement_cost'u 14.99 olan filmler seçildi
+-- SELECT * FROM film WHERE replacement_cost = 14.99;
+
+-- rental_rate'i 0.99 olan filmler seçildi
+-- SELECT * FROM film WHERE rental_rate = 0.99;
+
+-- rental_rate'i 0.99 olan filmlerin title ve length'i seçildi
+-- SELECT title,length FROM film WHERE rental_rate = 0.99;
+
+-- first_name'i Penelope olanlar seçildi
+-- SELECT * FROM actor WHERE first_name = 'Penelope';
+
+-- SELECT * FROM film WHERE rental_rate <= 1
+
+-- SELECT * FROM film WHERE rental_rate != 0.99
+
