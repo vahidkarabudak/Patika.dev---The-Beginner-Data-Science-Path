b12530e0b6fb8bd4ee56ee9c52e44dbeec9dd3d7
Vahid KARABUDAK <vahidkarabudakitu@gmail.com>
2022-10-08 03:50:21 +0300

    Create 8.Assignment.sql

---
+++ SQL/Assignments/8.Assignment.sql
@@ -0,0 +1,17 @@
+-- 8.Assignment for SQL
+
+/*
+1.Let's create a table running in your test database with column information id(INTEGER), name VARCHAR(50), birthday DATE, email VARCHAR(100).
+2.Let's add 50 pieces of data to the employee table we created using the 'Mockaroo' service.
+3.Let's do 5 UPDATE operations that will update the other columns according to each of the columns.
+4.Let's do 5 DELETE operations that will delete the relevant row according to each of the columns.
+*/
+
+-- Solution 1
+
+
+-- Solution 2
+
+-- Solution 3
+
+-- Solution 4
